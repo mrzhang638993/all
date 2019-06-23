a0e0da7d92413cc65995c07b0fe66a087d80f9fe
mrzhang638993 <1446719386@qq.com>
2019-06-23 11:37:55 +0800

    netty    commit

---
+++ netty/src/main/java/com/self/study/netty/EchoServerHandler.java
@@ -0,0 +1,26 @@
+package com.self.study.netty;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        System.out.println("收到客户端数据，还给客户端：" + msg);
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
