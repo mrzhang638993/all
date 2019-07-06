c52180e4a190261cf70060f01365ca1892b0362e
mrzhang638993 <1446719386@qq.com>
2019-07-06 10:26:33 +0800

    socket.readline  存在很大的坑的，不推荐使用的。

--- netty/src/main/java/com/self/study/study/netty/socket/Server.java
+++ netty/src/main/java/com/self/study/study/netty/socket/Server.java
@@ -15,6 +15,7 @@ public class Server {
             BufferedReader  bufferedReader= new BufferedReader(new InputStreamReader(is));
             //  阻塞方法，需要等待客户端的输入的，无输入的话，会一直等待的。
             String msg=null;
+            //  readline对应的会阻塞的，导致对应的只能够接受到一个客户端的请求的。
             while ((msg= bufferedReader.readLine())!=null){
                 System.out.println(msg);
             }
