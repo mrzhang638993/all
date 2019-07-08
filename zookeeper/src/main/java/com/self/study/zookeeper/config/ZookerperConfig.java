d01d5b55868af9b39a2e3f26f9ea58220fbf67de
mrzhang638993 <1446719386@qq.com>
2019-07-08 14:20:52 +0800

    curator   相关的zk实现

---
+++ zookeeper/src/main/java/com/self/study/zookeeper/config/ZookerperConfig.java
@@ -0,0 +1,35 @@
+package com.self.study.zookeeper.config;
+
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
+import org.apache.curator.CuratorZookeeperClient;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ZookerperConfig {
+
+    @Bean
+    public ZkClient zkClient() {
+        ZkClient zkClient = new ZkClient("localhost", 12000);
+        //  可以进行对象的序列化操作的
+        zkClient.setZkSerializer(new SerializableSerializer());
+        return zkClient;
+    }
+
+    @Bean
+    public CuratorFramework curatorFramework() {
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString("localhost:2181").connectionTimeoutMs(12000)
+                .retryPolicy(new RetryNTimes(2,5))
+                .build();
+        //  必须要启动start ，否则存在异常的。状态无法检测的。
+        curatorFramework.start();
+         return  curatorFramework;
+    }
+
+}
---
+++ zookeeper/src/main/java/com/self/study/zookeeper/vo/User.java
@@ -0,0 +1,36 @@
+package com.self.study.zookeeper.vo;
+
+import java.io.Serializable;
+
+public class User  implements Serializable {
+
+
+    private   int  age;
+
+    private   String name ;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
