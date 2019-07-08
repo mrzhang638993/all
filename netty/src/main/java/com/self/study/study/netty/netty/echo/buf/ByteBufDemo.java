1ba6d500f5defe7e59790b39fb059af7946bd09d
mrzhang638993 <1446719386@qq.com>
2019-07-08 10:58:07 +0800

    bytebuf相关代码

--- netty/src/main/java/com/self/study/study/netty/netty/echo/buf/ByteBufDemo.java
+++ netty/src/main/java/com/self/study/study/netty/netty/echo/buf/ByteBufDemo.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 public class ByteBufDemo {
 
     public static void main(String[] args) {
-
         // 初始化256字节的bytebuf,对应的是非池化的bytebuf的
         ByteBuf buffer = Unpooled.buffer(10);
         byte []  input=new  byte[]{1,2,3,4,5};
@@ -26,6 +25,20 @@ public class ByteBufDemo {
         buffer.discardReadBytes();
         System.out.println("元素清除之后，"+Arrays.toString(buffer.array()) );
 
+        //  清空读写指针之后实现相关操作
+        buffer.clear();
+        System.out.println("清空读写指针之后，对应的元素信息如下："+Arrays.toString(buffer.array()) );
+        byte[]  bytes= new  byte[]{5,6,7,8,9};
+        buffer.writeBytes(bytes);
+        System.out.println("继续写入数据之后的信息如下："+Arrays.toString(buffer.array()) );
+
+
+        //  清空所有的元素信息
+        buffer.setZero(0,buffer.capacity());
+        System.out.println("全部元素设置为0之后："+Arrays.toString(buffer.array()) );
+        byte[]   newInput= new  byte[]{11,12,13,14,15};
+        buffer.writeBytes(newInput);
+        System.out.println("数据插入之后的操作信息如下："+Arrays.toString(buffer.array()) );
 
 
     }
