d000a70d77ec80a67f465717ae5ce5b92a7fcf2f
mrzhang638993 <1446719386@qq.com>
2019-07-09 09:50:19 +0800

    使用zk的分布式锁来操作相关的实现信息

--- zookeeper/src/test/java/com/self/study/zookeeper/ZookeeperApplicationTests.java
+++ zookeeper/src/test/java/com/self/study/zookeeper/ZookeeperApplicationTests.java
@@ -1,19 +1,20 @@
 package com.self.study.zookeeper;
 
+import com.self.study.zookeeper.lock.ImproveLock;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ZookeeperApplicationTests {
 
-    private int result = 0;
+    private AtomicInteger result = new AtomicInteger(0);
     @Test
     public void contextLoads() {
     }
@@ -34,8 +35,8 @@ public class ZookeeperApplicationTests {
                 public void run() {
                     try {
                         lock.acquire();
-                        result++;
-                        System.out.println("获取到了互斥锁:{}" + result );
+                       result.getAndIncrement();
+                        System.out.println("获取到了互斥锁:{}" + result.get() );
                     } catch (Exception e) {
                         e.printStackTrace();
                     } finally {
@@ -51,6 +52,43 @@ public class ZookeeperApplicationTests {
         }
         try {
             Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Autowired
+     private  ImproveLock  improveLock;
+
+
+    //  测试zk的分布式锁
+    @Test
+    public  void    testZookeeperDistribute(){
+        for (int  i=0;i<100;i++){
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        improveLock.lock();
+
+                        result.getAndIncrement();
+                        System.out.println("获取到了互斥锁:{}" + result.get() );
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            improveLock.unlock();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            thread.start();
+        }
+        try {
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
