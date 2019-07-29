0fa0916c2eebf51a986ad53d923698d5bec8b21a
mrzhang638993 <1446719386@qq.com>
2019-07-29 23:03:27 +0800

    增加pageHelper相关说明信息

--- mybatis/src/main/java/com/self/study/mapper/UserEntityMapper.java
+++ mybatis/src/main/java/com/self/study/mapper/UserEntityMapper.java
@@ -5,11 +5,8 @@ import com.self.study.domain.OrderEntity;
 import com.self.study.domain.UserEntity;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
 
-import java.util.List;
-
-
+//  使用继承Mapper 这些操作的话，不要使用自定义的方法的，存在问题的。
 @Repository
 public interface UserEntityMapper  extends Mapper<UserEntity> {
 
